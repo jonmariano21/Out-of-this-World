a9e6161e95d507a353e58aa7c0b616c8e0a9c1d2
jonmariano21 <jonmariano21@gmail.com>
2014-06-19 15:28:43 -0700

    Initial Commit

---
+++ Out of this World/JMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JMAppDelegate.h
+//  Out of this World
+//
+//  Created by JONATHAN MARIANO on 6/19/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
